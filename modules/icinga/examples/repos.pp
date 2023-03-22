5b8ded6f30b76b8fabd3b58f519f7bf1348f6b19
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-03-22 17:14:20 -0600

    Remove Icinga

--- modules/icinga/examples/repos.pp
+++
@@ -1,5 +0,0 @@
-class { 'icinga::repos':
-  manage_epel              => true,
-  manage_powertools        => true,
-  manage_server_monitoring => true,
-}
--- modules/icinga/types/loglevel.pp
+++
@@ -1,2 +0,0 @@
-# A strict type for log levels
-type Icinga::LogLevel = Enum['debug', 'information', 'notice', 'warning', 'critical']
