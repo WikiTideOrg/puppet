d796f5390dd8cd5354184b5c6d19568b33add7d8
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-03-22 14:28:35 -0600

    Initial setup of puppet1 and mw1

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,95 +1,7 @@
 # servers
 
-node /^bast1[24]1\.miraheze\.org$/ {
-    include base
-    include role::bastion
-}
-
-node /^cloud1[01234]\.miraheze\.org$/ {
-    include base
-    include role::cloud
-}
-
-node /^cp(2[23]|3[23])\.miraheze\.org$/ {
-    include base
-    include role::varnish
-}
-
-node /^db1([0234][12]|12)\.miraheze\.org$/ {
-    include base
-    include role::db
-}
-
-node /^es1[34]1\.miraheze\.org$/ {
-    include base
-    include role::elasticsearch
-}
-
-node 'graylog121.miraheze.org' {
-    include base
-    include role::graylog
-}
-
-node 'jobchron121.miraheze.org' {
-    include base
-    include role::redis
-    include mediawiki::jobqueue::chron
-}
-
-node 'ldap141.miraheze.org' {
-    include base
-    include role::openldap
-}
-
-node 'mail121.miraheze.org' {
-    include base
-    include role::mail
-    include role::roundcubemail
-}
-
-node 'matomo131.miraheze.org' {
-    include base
-    include role::matomo
-}
-
-node /^mem1[34]1\.miraheze\.org$/ {
-    include base
-    include role::memcached
-}
-
-node 'mon141.miraheze.org' {
-    include base
-    include role::grafana
-    include role::icinga2
-    include role::irc
-}
-
-node /^mw1[234][12]\.miraheze\.org$/ {
-    include base
-    include role::mediawiki
-}
-
-node 'mwtask141.miraheze.org' {
-    include base
-    include role::mediawiki
-}
-
-node /^ns[12]\.miraheze\.org$/ {
-    include base
-    include role::dns
-}
-
-node 'phab121.miraheze.org' {
-    include base
-    include role::phabricator
-}
-
-node 'prometheus131.miraheze.org' {
-    include base
-    include role::prometheus
-}
-
-node 'puppet141.miraheze.org' {
+# puppet1
+node 'ec2-3-16-47-25.us-east-2.compute.amazonaws.com' {
     include base
     include role::postgresql
     include puppetdb::database
@@ -98,31 +10,10 @@ node 'puppet141.miraheze.org' {
     include role::ssl
 }
 
-node 'reports121.miraheze.org' {
-    include base
-    include role::reports
-}
-
-node /^swiftproxy1[13]1\.miraheze\.org$/ {
-    include base
-    include role::swift
-}
-
-node 'swiftac111.miraheze.org' {
-    include base
-    include role::swift
-}
-
-node /^swiftobject1[12][123]\.miraheze\.org$/ {
-    include base
-    include role::swift
-}
-
-node 'test131.miraheze.org' {
+# mw1
+node 'ec2-18-221-90-68.us-east-2.compute.amazonaws.com' {
     include base
     include role::mediawiki
-    include role::redis
-    include mediawiki::jobqueue::chron
 }
 
 # ensures all servers have basic class if puppet runs
