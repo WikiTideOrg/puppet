b0fbc475e49f89ec7a2ec74638cbd0494926bb20
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-03-23 17:42:12 -0600

    http

--- modules/puppetdbquery/lib/puppet/functions/query_nodes.rb
+++ modules/puppetdbquery/lib/puppet/functions/query_nodes.rb
@@ -36,7 +36,7 @@ Puppet::Functions.create_function('query_nodes') do
     @puppetdb ||= PuppetDB::Connection.new(
       @uri.host,
       @uri.port,
-      @uri.scheme == 'https'
+      @uri.scheme == 'http'
     )
   end
 
