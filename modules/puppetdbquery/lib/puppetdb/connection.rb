4aa97ef2eaeb922a0d10e142ed52f8308f297baa
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-03-23 19:16:16 -0600

    SSL

--- modules/puppetdbquery/lib/puppetdb/connection.rb
+++ modules/puppetdbquery/lib/puppetdb/connection.rb
@@ -9,7 +9,7 @@ class PuppetDB::Connection
 
   include Puppet::Util::Logging
 
-  def initialize(host = 'puppetdb', port = 80, use_ssl = false)
+  def initialize(host = 'puppetdb', port = 443, use_ssl = true)
     @host = host
     @port = port
     @use_ssl = use_ssl
