2d34693ec7ecabbee9670f496068615e61da0d4c
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-03-24 12:03:14 -0600

    base::dns remove monitoring

--- modules/base/manifests/dns.pp
+++ modules/base/manifests/dns.pp
@@ -17,12 +17,6 @@ class base::dns {
         require => Package['pdns-recursor'],
     }
 
-    monitoring::nrpe { 'PowerDNS Recursor':
-        command  => '/usr/lib/nagios/plugins/check_dns -s 127.0.0.1 -H miraheze.org',
-        docs     => 'https://meta.miraheze.org/wiki/Tech:Icinga/Base_Monitoring#PowerDNS_Recursor',
-        critical => true
-    }
-
     file { '/etc/resolv.conf':
         content => template('base/dns/resolv.conf.erb'),
         require => Package['pdns-recursor'],
