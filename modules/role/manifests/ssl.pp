f5b67606520f09c8e355504083cfa6fc2b593fa9
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-03-23 19:52:57 -0600

    -Firewall temp

--- modules/role/manifests/ssl.pp
+++ modules/role/manifests/ssl.pp
@@ -2,35 +2,6 @@
 class role::ssl {
     include ::ssl
 
-    $firewall_srange = join(
-        query_facts('Class[Role::Varnish] or Class[Role::Icinga2]', ['ipaddress', 'ipaddress6'])
-        .map |$key, $value| {
-            "${value['ipaddress']} ${value['ipaddress6']}"
-        }
-        .flatten()
-        .unique()
-        .sort(),
-        ' '
-    )
-
-    if !defined(Ferm::Service['http']) {
-        ferm::service { 'http':
-            proto   => 'tcp',
-            port    => '80',
-            srange  => "(${firewall_srange})",
-            notrack => true,
-        }
-    }
-
-    if !defined(Ferm::Service['https']) {
-        ferm::service { 'https':
-            proto   => 'tcp',
-            port    => '443',
-            srange  => "(${firewall_srange})",
-            notrack => true,
-        }
-    }
-
     @@sshkey { 'github.com':
         ensure       => present,
         type         => 'ecdsa-sha2-nistp256',
