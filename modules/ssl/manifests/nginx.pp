d7eefa182e341a7951ab813cbf240c4da38fa015
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-03-24 13:32:00 -0600

    Remove monitor option

--- modules/ssl/manifests/nginx.pp
+++ modules/ssl/manifests/nginx.pp
@@ -5,7 +5,6 @@ class ssl::nginx {
     nginx::site { 'ssl-acme':
         ensure  => present,
         source  => 'puppet:///modules/ssl/nginx.conf',
-        monitor => false,
     }
 
     ssl::wildcard { 'ssl-acme nginx wildcard': }
