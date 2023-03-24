0dbec8648b8f792d30f4987bffb07987188161ec
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-03-23 22:29:22 -0600

    iaupload: use wikiforge

--- modules/mediawiki/files/bin/iaupload.py
+++ modules/mediawiki/files/bin/iaupload.py
@@ -6,12 +6,6 @@ import os
 
 from datetime import datetime
 
-# set HTTP proxy to use for getting the item from archive.org
-# we then also set the session proxy for the item to use for uploading
-# but we can't get the item to set session proxy without also setting HTTP_PROXY here
-os.environ['HTTP_PROXY'] = 'http://bast.miraheze.org:8080'
-os.environ['HTTPS_PROXY'] = 'http://bast.miraheze.org:8080'
-
 # add arguments
 parser = argparse.ArgumentParser(
     description='Uploads a file to archive.org.')
@@ -25,8 +19,8 @@ parser.add_argument(
     '--mediatype', dest='mediatype', default='web',
     help='The media type of the file to be used on archive.org. Optional. Default: web')
 parser.add_argument(
-    '--subject', dest='subject', default='miraheze;wikiteam',
-    help='Subject (topics) of the file for archive.org. Multiple topics can be separated by a semicolon. Optional. Default: miraheze;wikiteam')
+    '--subject', dest='subject', default='wikiforge;wikiteam',
+    help='Subject (topics) of the file for archive.org. Multiple topics can be separated by a semicolon. Optional. Default: wikiforge;wikiteam')
 parser.add_argument(
     '--collection', dest='collection', default='opensource',
     help='The name of the collection to use on archive.org. Optional. Default: opensource')
@@ -36,12 +30,6 @@ parser.add_argument(
 args = parser.parse_args()
 
 item = internetarchive.get_item(args.title)
-
-# set session proxy for uploading
-item.session.proxies = {
-    'http': 'http://bast.miraheze.org:8080',
-    'https': 'http://bast.miraheze.org:8080',
-}
 
 # get last modification time from file to use as the publication date in archive.org
 mtime = os.path.getmtime(args.file)
