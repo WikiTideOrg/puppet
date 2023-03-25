294187645abbfde43ab183f5b95feaf2a1000073
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-03-24 22:50:11 -0600

    Use Single quotes

--- modules/mediawiki/templates/PrivateSettings.php
+++ modules/mediawiki/templates/PrivateSettings.php
@@ -1,25 +1,25 @@
 <?php
 
 // Database passwords
-$wgDBadminpassword = "<%= @wikiadmin_password %>";
-$wgDBpassword = "<%= @mediawiki_password %>";
+$wgDBadminpassword = '<%= @wikiadmin_password %>';
+$wgDBpassword = '<%= @mediawiki_password %>';
 
 // Redis AUTH password
-$wmgRedisPassword = "<%= @redis_password %>";
+$wmgRedisPassword = '<%= @redis_password %>';
 
 // Noreply authentication password
-$wmgSMTPPassword = "<%= @noreply_password %>";
+$wmgSMTPPassword = '<%= @noreply_password %>';
 
 // MediaWiki secret keys
-$wgUpgradeKey = "<%= @mediawiki_upgradekey %>";
-$wgSecretKey = "<%= @mediawiki_secretkey %>";
+$wgUpgradeKey = '<%= @mediawiki_upgradekey %>';
+$wgSecretKey = '<%= @mediawiki_secretkey %>';
 
 // hCaptcha secret key
-$wgHCaptchaSecretKey = "<%= @hcaptcha_secretkey %>";
+$wgHCaptchaSecretKey = '<%= @hcaptcha_secretkey %>';
 
 // Shellbox secret key
-$wgShellboxSecretKey = "<%= @shellbox_secretkey %>";
+$wgShellboxSecretKey = '<%= @shellbox_secretkey %>';
 
 // Extension:DiscordNotifications global webhook
-$wmgGlobalDiscordWebhookUrl = "<%= @global_discord_webhook_url %>";
-$wmgDiscordExperimentalWebhook = "<%= @discord_experimental_webhook %>";
+$wmgGlobalDiscordWebhookUrl = '<%= @global_discord_webhook_url %>';
+$wmgDiscordExperimentalWebhook = '<%= @discord_experimental_webhook %>';
