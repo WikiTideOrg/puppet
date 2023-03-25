c3a4d27515c39b74bd174c8f9a60db10011808fa
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-03-25 00:02:40 -0600

    Switch repo

--- modules/irc/manifests/relaybot.pp
+++ modules/irc/manifests/relaybot.pp
@@ -31,7 +31,7 @@ class irc::relaybot {
 
     git::clone { 'IRC-Discord-Relay':
         ensure    => latest,
-        origin    => 'https://github.com/Universal-Omega/IRC-Discord-Relay.git',
+        origin    => 'https://github.com/WikiForge/IRC-Discord-Relay.git',
         directory => $install_path,
         owner     => 'irc',
         group     => 'irc',
