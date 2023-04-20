a9ce6e683771ac03d70b3ccd0d8f2f71e30e3d2d
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-04-20 14:01:38 -0600

    Ensure /usr/share/GeoIP exists

--- modules/dns/manifests/init.pp
+++ modules/dns/manifests/init.pp
@@ -14,10 +14,16 @@ class dns {
         notify    => Exec['gdnsd-syntax'],
     }
 
+    file { '/usr/share/GeoIP':
+        ensure => directory,
+        mode   => '0444',
+    }
+
     file { '/usr/share/GeoIP/GeoLite2-Country.mmdb':
         ensure => present,
         source => 'puppet:///private/geoip/GeoLite2-Country.mmdb',
         mode   => '0444',
+        require => File['/usr/share/GeoIP'],
         notify => Exec['gdnsd-syntax'],
     }
 
