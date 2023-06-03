365dd57f1ae476bd2eac00c2b3ba5f98d407be31
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-06-02 23:11:54 -0600

    wikiforge-backup: remove mediawiki-static

--- modules/base/manifests/backup.pp
+++ modules/base/manifests/backup.pp
@@ -3,15 +3,6 @@ class base::backup (
     String $pca_password = lookup('private::passwords::pca')
 ) {
     ensure_packages(['python3-fabric', 'python3-decorator'])
-    ensure_packages(
-        'py7zr',
-        {
-            ensure   => '0.20.5',
-            provider => 'pip3',
-            before   => File['/usr/local/bin/wikiforge-backup'],
-            require  => Package['python3-pip'],
-        },
-    )
 
     file { '/usr/local/bin/wikiforge-backup':
         mode    => '0555',
