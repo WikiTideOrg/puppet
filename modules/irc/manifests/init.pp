db2387d01f7ba3e3e1ef5924022603e29ce9c23e
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-07-16 21:51:44 -0600

    logbot: support SASL authentication

--- modules/irc/manifests/init.pp
+++ modules/irc/manifests/init.pp
@@ -3,8 +3,16 @@ class irc {
     ensure_packages([
         'python3',
         'python3-twisted',
-        'python3-irc',
         'python3-requests',
         'python3-requests-oauthlib',
     ])
+
+    ensure_packages(
+        'git+https://github.com/anisse/irc',
+        {
+            ensure   => present,
+            provider => 'pip3',
+            require  => Package['python3-pip'],
+        },
+    )
 }
