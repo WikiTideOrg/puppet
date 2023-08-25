957dfa62b9d46676ad7cd9dae43aa0645175be5d
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2023-08-25 13:21:45 -0500

    Fix path URL

--- modules/phorge/files/wikiforge.conf.php
+++ modules/phorge/files/wikiforge.conf.php
@@ -3,6 +3,6 @@
 return array(
     'storage.s3.bucket' => 'phorge-storage.wikiforge.net',
     'phabricator.base-uri' => "https://support.wikiforge.net/",
-    'security.alternate-file-domain' => "https://phorge-storage.wikiforge.net/",
+    'security.alternate-file-domain' => "https://phorge-static.wikiforge.net/",
     'storage.default-namespace' => 'phorge'
 );
