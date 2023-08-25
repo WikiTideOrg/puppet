2a916bf8c21f8686c22f66ae56a89210e2890a1d
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2023-08-25 12:15:13 -0500

    Notify both phd services

--- modules/phorge/manifests/init.pp
+++ modules/phorge/manifests/init.pp
@@ -211,21 +211,30 @@ class phorge (
     file { '/srv/phorge/phorge/conf/local/local.json':
         ensure  => present,
         content => to_json_pretty($phorge_settings),
-        notify  => Service['phd'],
+        notify  => [
+            Service['phd-wikiforge'],
+            Service['phd-wikitide'],
+        ],
         require => Git::Clone['phorge'],
     }
 
     file { '/srv/phorge/phorge/conf/custom/wikiforge.conf.php':
         ensure  => present,
         content => 'puppet:///modules/phorge/data/wikiforge.conf.php',
-        notify  => Service['phd'],
+        notify  => [
+            Service['phd-wikiforge'],
+            Service['phd-wikitide'],
+        ],
         require => Git::Clone['phorge'],
     }
 
     file { '/srv/phorge/phorge/conf/custom/wikitide.conf.php':
         ensure  => present,
         content => 'puppet:///modules/phorge/data/wikitide.conf.php',
-        notify  => Service['phd'],
+        notify  => [
+            Service['phd-wikiforge'],
+            Service['phd-wikitide'],
+        ],
         require => Git::Clone['phorge'],
     }
 
