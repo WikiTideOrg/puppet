29528f50bd9dcbff8b8c624d2784998caa07022e
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2023-08-25 12:33:38 -0500

    Change content to source

--- modules/phorge/data/wikiforge.conf.php
+++ modules/phorge/data/wikiforge.conf.php
@@ -2,7 +2,7 @@
 
 return array(
     'storage.s3.bucket' => 'phorge-storage.wikiforge.net',
-    'phabricator.base-uri' => 'https://support.wikiforge.net/',
-    'security.alternate-file-domain' => 'https://phorge-storage.wikiforge.net/',
+    'phabricator.base-uri' => "https://support.wikiforge.net/",
+    'security.alternate-file-domain' => "https://phorge-storage.wikiforge.net/",
     'storage.default-namespace' => 'phorge'
 );
