ad8748722296c58ca9555575959ba7fe7d201477
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2023-08-25 21:29:29 -0500

    Fix syntax

--- modules/phorge/files/archive.conf.php
+++ modules/phorge/files/archive.conf.php
@@ -3,6 +3,6 @@
 return array(
     'phabricator.base-uri' => "https://support-archive.wikiforge.net/",
     'security.alternate-file-domain' => "https://phorge-static.wikiforge.net/",
-    'storage.default-namespace' => 'phorge'
+    'storage.default-namespace' => 'phorge',
     'storage.s3.bucket' => 'phorge-storage.wikiforge.net',
 );
