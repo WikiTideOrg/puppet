ab837c621e8e10f4d56081f9cef6281a515a1a9a
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2023-08-25 21:30:34 -0500

    Fix syntax

--- modules/phorge/files/wikiforge.conf.php
+++ modules/phorge/files/wikiforge.conf.php
@@ -4,6 +4,6 @@ return array(
     'phabricator.base-uri' => "https://support.wikiforge.net/",
     'repository.default-local-path' => "/srv/phorge/repos/wikiforge/"
     'security.alternate-file-domain' => "https://phorge-static.wikiforge.net/",
-    'storage.default-namespace' => 'wfphorge'
+    'storage.default-namespace' => 'wfphorge',
     'storage.s3.bucket' => 'phorge-storage.wikiforge.net',
 );
