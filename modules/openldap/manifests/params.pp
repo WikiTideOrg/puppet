345e63061b13fd94e813e8aa9356bedd9ffec840
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2023-08-27 23:51:23 -0500

    Remove unused code

--- modules/openldap/manifests/params.pp
+++ modules/openldap/manifests/params.pp
@@ -1,7 +1,5 @@
 # See README.md for details.
 class openldap::params {
-  case $::osfamily {
-    'Debian': {
       $client_package           = 'libldap-2.4-2'
       $client_conffile          = '/etc/ldap/ldap.conf'
       $server_confdir           = '/etc/ldap/slapd.d'
@@ -18,50 +16,8 @@ class openldap::params {
       $utils_package            = 'ldap-utils'
       $escape_ldapi_ifs         = false
     }
-    'RedHat': {
-      $client_package           = 'openldap'
-      $client_conffile          = '/etc/openldap/ldap.conf'
-      $server_confdir           = '/etc/openldap/slapd.d'
-      $server_conffile          = '/etc/openldap/slapd.conf'
-      $server_group             = 'ldap'
-      $server_owner             = 'ldap'
-      $server_package           = 'openldap-servers'
-      $server_service           = $::operatingsystemmajrelease ? {
-        '5' => 'ldap',
-        default => 'slapd',
-      }
-      $server_service_hasstatus = true
-      $utils_package            = 'openldap-clients'
-      $escape_ldapi_ifs         = false
-    }
-    'Archlinux': {
-      $client_package           = 'openldap'
-      $client_conffile          = '/etc/openldap/ldap.conf'
-      $server_confdir           = '/etc/openldap/slapd.d'
-      $server_conffile          = '/etc/openldap/slapd.conf'
-      $server_group             = 'ldap'
-      $server_owner             = 'ldap'
-      $server_package           = 'openldap'
-      $server_service           = 'slapd'
-      $server_service_hasstatus = true
-      $utils_package            = undef
-      $escape_ldapi_ifs         = false
-    }
-    'FreeBSD': {
-      $client_package           = 'openldap-sasl-client'
-      $client_conffile          = '/usr/local/etc/openldap/ldap.conf'
-      $server_confdir           = '/usr/local/etc/openldap/slapd.d'
-      $server_conffile          = '/usr/local/etc/openldap/slapd.conf'
-      $server_group             = 'ldap'
-      $server_owner             = 'ldap'
-      $server_package           = 'openldap-sasl-server'
-      $server_service           = 'slapd'
-      $server_service_hasstatus = true
-      $utils_package            = undef
-      $escape_ldapi_ifs         = true
-    }
     default: {
-      fail "Operating System family ${::osfamily} not supported"
+      fail "Operating System family not supported"
     }
   }
 }
