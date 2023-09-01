c49ea77bd2240f1f7c169ca8d7b4157da4efbcab
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2023-09-01 09:45:55 -0500

    Fix syntax

--- modules/phorge/files/archive.conf.php
+++ modules/phorge/files/archive.conf.php
@@ -2,8 +2,8 @@
 
 return array(
     'metamta.default-address' => 'noreply@wikiforge.net',
-    'phabricator.base-uri' => "https://support-archive.wikiforge.net/",
-    'security.alternate-file-domain' => "https://phorge-static.wikiforge.net/",
+    'phabricator.base-uri' => 'https://support-archive.wikiforge.net/',
+    'security.alternate-file-domain' => 'https://phorge-static.wikiforge.net/',
     'storage.default-namespace' => 'phorge',
     'storage.s3.bucket' => 'phorge-storage.wikiforge.net',
 );
--- modules/phorge/files/wikiforge.conf.php
+++ modules/phorge/files/wikiforge.conf.php
@@ -2,9 +2,9 @@
 
 return array(
     'metamta.default-address' => 'noreply@wikiforge.net',
-    'phabricator.base-uri' => "https://support.wikiforge.net/",
-    'repository.default-local-path' => "/srv/phorge/repos/wikiforge/",
-    'security.alternate-file-domain' => "https://phorge-static.wikiforge.net/",
+    'phabricator.base-uri' => 'https://support.wikiforge.net/',
+    'repository.default-local-path' => '/srv/phorge/repos/wikiforge/',
+    'security.alternate-file-domain' => 'https://phorge-static.wikiforge.net/',
     'storage.default-namespace' => 'wfphorge',
     'storage.s3.bucket' => 'phorge-storage.wikiforge.net',
 );
