878350ac63d4100430f53e9b71b67064f10eda7b
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2023-09-01 09:41:47 -0500

    Phorge: Add comma to config

--- modules/phorge/files/wikiforge.conf.php
+++ modules/phorge/files/wikiforge.conf.php
@@ -3,7 +3,7 @@
 return array(
     'metamta.default-address' => 'noreply@wikiforge.net',
     'phabricator.base-uri' => "https://support.wikiforge.net/",
-    'repository.default-local-path' => "/srv/phorge/repos/wikiforge/"
+    'repository.default-local-path' => "/srv/phorge/repos/wikiforge/",
     'security.alternate-file-domain' => "https://phorge-static.wikiforge.net/",
     'storage.default-namespace' => 'wfphorge',
     'storage.s3.bucket' => 'phorge-storage.wikiforge.net',
--- modules/phorge/files/wikitide.conf.php
+++ modules/phorge/files/wikitide.conf.php
@@ -3,7 +3,7 @@
 return array(
     'metamta.default-address' => 'noreply@wikitide.com',
     'phabricator.base-uri' => "https://issue-tracker.wikitide.com/",
-    'repository.default-local-path' => "/srv/phorge/wikitide/repos/"
+    'repository.default-local-path' => "/srv/phorge/wikitide/repos/",
     'security.alternate-file-domain' => "https://phorge-static.wikitide.com/",
     'storage.default-namespace' => 'wtphorge',
     'storage.s3.bucket' => 'wikitide-phorge-storage',
