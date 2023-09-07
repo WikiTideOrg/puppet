e129309fdc4a1a5938f56c0705fc1951ec026341
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2023-09-06 23:24:14 -0500

    Phorge: Use wikitide.org

--- modules/phorge/files/wikitide.conf.php
+++ modules/phorge/files/wikitide.conf.php
@@ -2,9 +2,9 @@
 
 return array(
     'metamta.default-address' => 'noreply@wikitide.com',
-    'phabricator.base-uri' => 'https://issue-tracker.wikitide.com/',
+    'phabricator.base-uri' => 'https://issue-tracker.wikitide.org/',
     'repository.default-local-path' => '/srv/phorge/wikitide/repos/',
-    'security.alternate-file-domain' => 'https://phorge-static.wikitide.com/',
+    'security.alternate-file-domain' => 'https://phorge-static.wikitide.org/',
     'storage.default-namespace' => 'wtphorge',
     'storage.s3.bucket' => 'wikitide-phorge-storage',
 );
