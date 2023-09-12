05ff4a5406db0d0eaa6597d697397257b634565a
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2023-09-11 19:45:36 -0500

    mariadb: bump to 10.11

--- modules/mariadb/manifests/packages.pp
+++ modules/mariadb/manifests/packages.pp
@@ -1,6 +1,6 @@
 # class: mariadb::packages
 class mariadb::packages(
-    VMlib::Mariadb_version $version = lookup('mariadb::version', {'default_value' => '10.5'}),
+    VMlib::Mariadb_version $version = lookup('mariadb::version', {'default_value' => '10.11'}),
 ) {
 
     package { [
@@ -8,24 +8,6 @@ class mariadb::packages(
         'percona-toolkit',
     ]:
         ensure => present,
-    }
-
-    apt::source { 'mariadb_apt':
-        comment  => 'MariaDB stable',
-        location => "http://ams2.mirrors.digitalocean.com/mariadb/repo/${version}/debian",
-        release  => $facts['os']['distro']['codename'],
-        repos    => 'main',
-        key      => {
-                'id'     => '177F4010FE56CA3336300305F1656F24C74CD1D8',
-                'server' => 'hkp://keyserver.ubuntu.com:80',
-        },
-    }
-
-    apt::pin { 'mariadb_pin':
-        priority => 600,
-        origin   => 'ams2.mirrors.digitalocean.com',
-        require  => Apt::Source['mariadb_apt'],
-        notify   => Exec['apt_update_mariadb'],
     }
 
     # First installs can trip without this
