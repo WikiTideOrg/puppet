2e08ad9f7949a5d2e70dddbd5c61b26cc44d9303
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2023-09-20 04:30:28 +0000

    Revert "Remove monitoring"
    This reverts commit 64e806b5ad5d140499e1461937a4ca5d78426769.

---
+++ modules/monitoring/files/scripts/ssl-renew.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -x
+
+if [ -z "$SERVICESTATE" ]
+then
+  SERVICESTATE=$1
+fi
+
+if [ -z "$SERVICESTATETYPE" ]
+then
+  SERVICESTATETYPE=$2
+fi
+
+if [ -z "$SERVICEDESC" ]
+then
+  SERVICEDESC=$3
+fi
+
+curl -X POST -H 'Content-type: application/json' --data "{
+  \"SERVICESTATE\": \"${SERVICESTATE}\",
+  \"SERVICESTATETYPE\": \"${SERVICESTATETYPE}\",
+  \"SERVICEDESC\": \"${SERVICEDESC}\"
+}" http://[2a10:6740::6:507]:5000/renew >> /var/log/icinga2/ssl-let.log 2>&1
