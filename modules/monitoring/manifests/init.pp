b8f784c95df4810f9428e4ebcf6fc6b4a4b9847f
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2023-09-23 03:49:03 +0000

    Ensure mariadb-client

--- modules/monitoring/manifests/init.pp
+++ modules/monitoring/manifests/init.pp
@@ -22,26 +22,6 @@ class monitoring (
         allowdupe => false,
     }
 
-    $http_proxy = lookup('http_proxy', {'default_value' => undef})
-    $version = lookup('mariadb::version', {'default_value' => '10.4'})
-    apt::source { 'mariadb_apt':
-        comment  => 'MariaDB stable',
-        location => "http://ams2.mirrors.digitalocean.com/mariadb/repo/${version}/debian",
-        release  => $::lsbdistcodename,
-        repos    => 'main',
-        key      => {
-                'id'      => '177F4010FE56CA3336300305F1656F24C74CD1D8',
-                'server'  => 'hkp://keyserver.ubuntu.com:80',
-        },
-    }
-
-    apt::pin { 'mariadb_pin':
-        priority => 600,
-        origin   => 'ams2.mirrors.digitalocean.com',
-        require  => Apt::Source['mariadb_apt'],
-        notify   => Exec['apt_update_mariadb'],
-    }
-
     # First installs can trip without this
     exec { 'apt_update_mariadb':
         command     => '/usr/bin/apt-get update',
@@ -50,10 +30,9 @@ class monitoring (
     }
 
     ensure_packages(
-        "mariadb-client-${version}",
+        "mariadb-client",
         {
             ensure  => present,
-            require => Apt::Source['mariadb_apt'],
         },
     )
 
