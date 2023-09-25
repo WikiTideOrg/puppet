0444d8ab175243c5cda39dbd1a7a84be759e5e54
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2023-09-25 13:00:44 -0500

    Add monitoring to jobchron

--- modules/mediawiki/manifests/jobqueue/chron.pp
+++ modules/mediawiki/manifests/jobqueue/chron.pp
@@ -20,4 +20,9 @@ class mediawiki::jobqueue::chron (
         subscribe => File['/srv/jobrunner/jobrunner.json'],
         restart   => true,
     }
+
+    monitoring::nrpe { 'JobChron Service':
+        command => '/usr/lib/nagios/plugins/check_procs -a redisJobChronService -c 1:1',
+        docs    => 'https://meta.miraheze.org/wiki/Tech:Icinga/MediaWiki_Monitoring#JobChron_Service'
+    }
 }
