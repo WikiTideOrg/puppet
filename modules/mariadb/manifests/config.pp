467f6c62fc769bc02cc6c8c726a39084e55dbaea
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2023-10-23 09:16:44 -0500

    mariadb: Add monitoring

--- modules/mariadb/manifests/config.pp
+++ modules/mariadb/manifests/config.pp
@@ -106,7 +106,7 @@ class mariadb::config(
             mysql_username => 'icinga',
             mysql_password => $icinga_password,
             mysql_ssl      => true,
-            mysql_cacert   => '/etc/ssl/certs/Sectigo.crt',
+            mysql_cacert   => '/etc/ssl/certs/ISRG_Root_X1.pem',
         },
     }
 
