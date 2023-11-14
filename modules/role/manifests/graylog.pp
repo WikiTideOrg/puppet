9feaca9c19d82c5f71be64dce338d9992ca5ea1f
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-11-13 23:27:10 -0700

    Set message_journal_max_size to 2GB

--- modules/role/manifests/graylog.pp
+++ modules/role/manifests/graylog.pp
@@ -31,6 +31,7 @@ class role::graylog {
             'root_password_sha2'  => lookup('passwords::graylog::root_password_sha2'),
             'elasticsearch_hosts' => $elasticsearch_host,
             'ignore_migration_failures' => true,
+            'message_journal_max_size' => '2gb',
         }
     }
 
