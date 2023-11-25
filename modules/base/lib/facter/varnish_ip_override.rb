ed5e1f6cc009bc281f0f0a10b137c727080100f7
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-11-25 16:50:53 -0700

    Fix

--- modules/base/lib/facter/varnish_ip_override.rb
+++ modules/base/lib/facter/varnish_ip_override.rb
@@ -1,39 +1,20 @@
-
 Facter.add('varnish_ip_override') do
   setcode do
     networking = Facter.value('networking')
 
-    if networking
-      if networking['interfaces']
-        if networking['interfaces']['ens19']
-          if networking['interfaces']['ens19']['ip']
-            varnish_role = Facter.value(:classification)['classes'] && Facter.value(:classification)['classes'].include?('Role::Varnish')
+    if networking && networking['interfaces'] && networking['interfaces']['ens19'] && networking['interfaces']['ens19']['ip']
+      varnish_role = Facter.value(:classification)['classes'] && Facter.value(:classification)['classes'].include?('Role::Varnish')
 
-            if varnish_role
-              # Return the overridden IP address for nodes with Role::Varnish
-              networking['interfaces']['ens19']['ip']
-            else
-              # Return the default IP address for other nodes
-              networking['ip']
-            end
-          else
-            # Print a debug message if 'ip' is nil
-            warn "The 'ip' key for 'ens19' is nil."
-            'fallback_value'
-          end
-        else
-          # Print a debug message if 'ens19' is nil
-          warn "The 'ens19' key in 'interfaces' is nil."
-          'fallback_value'
-        end
+      if varnish_role
+        # Return the overridden IP address for nodes with Role::Varnish
+        networking['interfaces']['ens19']['ip']
       else
-        # Print a debug message if 'interfaces' is nil
-        warn "The 'interfaces' key is nil."
-        'fallback_value'
+        # Return the default IP address for other nodes
+        networking['ip']
       end
     else
-      # Print a debug message if 'networking' is nil
-      warn "The 'networking' fact is nil."
+      # Print a debug message if 'networking' or its keys are nil
+      warn "The 'networking' fact or its keys are nil."
       'fallback_value'
     end
   end
