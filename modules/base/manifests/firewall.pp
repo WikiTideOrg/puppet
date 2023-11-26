43ec82ec0f4008f42abcfcb0c5728629c139768d
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-11-25 17:23:25 -0700

    Fix

--- modules/base/manifests/firewall.pp
+++ modules/base/manifests/firewall.pp
@@ -33,7 +33,11 @@ class base::firewall (
     $firewall_rules_str = join(
         query_facts("networking.domain='${facts['networking']['domain']}' and Class[Role::Icinga2]", ['networking'])
         .map |$key, $value| {
-            "${value['networking']['ip']} ${value['networking']['ip6']}"
+            if $value['networking']['interfaces']['ens19'] {
+                "${value['networking']['interfaces']['ens19']['ip']} ${value['networking']['interfaces']['ens19']['ip6']}"
+            } else {
+                "${value['networking']['ip']} ${value['networking']['ip6']}"
+            }
         }
         .flatten()
         .unique()
