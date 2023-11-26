1889d896285ce19fe1a434b508058efe60b85e7a
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-11-26 16:59:02 -0700

    Fix

--- modules/haproxy/types/tlscertificate.pp
+++ modules/haproxy/types/tlscertificate.pp
@@ -1,5 +1,5 @@
 type Haproxy::Tlscertificate = Struct[{
-    'server_names'       => Array[Variant[Wmflib::Host::Wildcard, Stdlib::Fqdn]],
+    'server_names'       => Array[Variant[VMlib::Host::Wildcard, Stdlib::Fqdn]],
     'cert_paths'         => Array[Stdlib::Unixpath],
     'warning_threshold'  => Optional[Integer[0]],
     'critical_threshold' => Optional[Integer[0]],
