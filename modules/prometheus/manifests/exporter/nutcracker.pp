f45a2076e1f4b19b8c674d11feecb247b47ea791
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-12-04 15:50:36 -0700

    Use deb
    Signed-off-by: CosmicAlpha <cosmic.alpha24@gmail.com>

--- modules/prometheus/manifests/exporter/nutcracker.pp
+++ modules/prometheus/manifests/exporter/nutcracker.pp
@@ -1,5 +1,15 @@
 class prometheus::exporter::nutcracker {
-    stdlib::ensure_packages('prometheus-nutcracker-exporter')
+    file { '/opt/prometheus-nutcracker-exporter_0.3_all.deb':
+        ensure => present,
+        source => 'puppet:///modules/prometheus/packages/prometheus-nutcracker-exporter_0.3_all.deb',
+    }
+
+    package { 'prometheus-nutcracker-exporter':
+        ensure   => installed,
+        provider => dpkg,
+        source   => '/opt/prometheus-nutcracker-exporter_0.3_all.deb',
+        require  => File['/opt/prometheus-nutcracker-exporter_0.3_all.deb'],
+    }
 
     service { 'prometheus-nutcracker-exporter':
         ensure  => running,
