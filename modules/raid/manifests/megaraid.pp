c4079a276dbdaf2617d071c68da934ba8c0f50bc
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-12-04 20:25:00 -0700

    Add megaraid
    Signed-off-by: CosmicAlpha <cosmic.alpha24@gmail.com>

---
+++ modules/raid/manifests/megaraid.pp
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: Apache-2.0
+# Dell PowerEdge RAID Controler
+class raid::perccli {
+    ensure_packages('perccli')
+
+    file { '/usr/lib/nagios/plugins/get-raid-status-perccli.py':
+        source  => 'puppet:///modules/raid/get-raid-status-perccli',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        require => Package['nagios-nrpe-plugin'],
+    }
+
+    monitoring::nrpe { 'get_raid_status_perccli':
+        command   => '/usr/local/lib/nagios/plugins/get-raid-status-perccli',
+        sudo_user => 'root',
+    }
+
+    monitoring::services { 'Dell PowerEdge RAID Controller':
+        check_command => '/usr/local/lib/nagios/plugins/get-raid-status-perccli',
+    }
+}
