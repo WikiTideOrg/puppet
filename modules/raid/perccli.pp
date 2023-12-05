93b40b7fd58fa03996b0a4405dace62b57f1602e
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-12-04 19:23:05 -0700

    Add
    Signed-off-by: CosmicAlpha <cosmic.alpha24@gmail.com>

--- modules/raid/perccli.pp
+++ modules/raid/perccli.pp
@@ -3,6 +3,14 @@
 class raid::perccli {
     ensure_packages('perccli')
 
+    file { '/usr/lib/nagios/plugins/get-raid-status-perccli.py':
+        source  => 'puppet:///modules/raid/get-raid-status-perccli',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+        require => Package['nagios-nrpe-plugin'],
+    }
+
     monitoring::nrpe { 'get_raid_status_perccli':
         command   => '/usr/local/lib/nagios/plugins/get-raid-status-perccli',
         sudo_user => 'root',
