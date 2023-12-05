da01ec47f660dcadf9bcf33c3f8ec1886987ddcc
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-12-05 14:28:38 -0700

    Use event_command

--- modules/raid/manifests/perccli.pp
+++ modules/raid/manifests/perccli.pp
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
 # Dell PowerEdge RAID Controler
 class raid::perccli {
+    include raid
+
     ensure_packages('perccli')
 
     file { '/usr/lib/nagios/plugins/get-raid-status-perccli.py':
@@ -16,6 +18,12 @@ class raid::perccli {
     }
 
     monitoring::services { 'Dell PowerEdge RAID Controller':
-        check_command => '/usr/local/lib/nagios/plugins/get-raid-status-perccli',
+        check_command  => '/usr/local/lib/nagios/plugins/get-raid-status-perccli',
+        check_interval => $raid::check_interval,
+        retry_interval => $raid::retry_interval,
+        event_command  => 'raid_handler',
+        vars           => {
+            raid_controller => 'perccli',
+        },
     }
 }
