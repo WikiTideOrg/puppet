8aba7622ea6e3c2076fd2105eacb61812924bfa7
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-12-08 14:06:49 -0700

    Use 'wiki' suffix for meta and commons and use databases.json directly (#42)
    * Use 'wiki' suffix for meta and commons and use databases.json directly
    
    * -tide
    
    * Fix

--- modules/mediawiki/files/bin/mwscript.py
+++ modules/mediawiki/files/bin/mwscript.py
@@ -41,7 +41,7 @@ def get_commands(args: argparse.Namespace) -> CommandInfo | int:
     try:
         if args.extension:
             wiki = ''
-        elif args.arguments[0].endswith('wikitide') or args.arguments[0].endswith('nexttide') or args.arguments[0] in [*['all', 'wikitide', 'nexttide'], *validDBLists]:
+        elif args.arguments[0].endswith('wiki') or args.arguments[0].endswith('nexttide') or args.arguments[0] in [*['all', 'wikitide', 'nexttide'], *validDBLists]:
             wiki = args.arguments[0]
             args.arguments.remove(wiki)
             if args.arguments == []:
@@ -96,7 +96,7 @@ def get_commands(args: argparse.Namespace) -> CommandInfo | int:
             except KeyboardInterrupt:
                 sys.exit()
             list_script = {
-                'wikitide': lambda: f'sudo -u www-data /usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases-wikitide.json {script}',
+                'wikitide': lambda: f'sudo -u www-data /usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases.json {script}',
                 'allfarms': lambda: f'sudo -u www-data /usr/local/bin/foreachwikiindblist /srv/mediawiki/cache/databases-all.json {script}',
             }
             command_choice = list_script.get(list_choice)
@@ -123,7 +123,7 @@ def get_commands(args: argparse.Namespace) -> CommandInfo | int:
             except KeyboardInterrupt:
                 return 2
             generate_script = {
-                'wikitide': lambda: f'php {runner}/srv/mediawiki/{args.version}/extensions/WikiTideMagic/maintenance/generateExtensionDatabaseList.php --wiki=metawikitide --extension={args.extension}',
+                'wikitide': lambda: f'php {runner}/srv/mediawiki/{args.version}/extensions/WikiTideMagic/maintenance/generateExtensionDatabaseList.php --wiki=metawiki --extension={args.extension}',
                 'nexttide': lambda: f'php {runner}/srv/mediawiki/{args.version}/extensions/WikiTideMagic/maintenance/generateExtensionDatabaseList.php --wiki=metanexttide --extension={args.extension}',
             }
             generate_choice = generate_script.get(farm_choice)
--- modules/varnish/templates/default.vcl
+++ modules/varnish/templates/default.vcl
@@ -885,7 +885,7 @@ sub vcl_backend_error {
 		<div class="container" style="padding: 70px 0; text-align: center;">
 			<!-- Jumbotron -->
 			<div class="jumbotron">
-				<img src="https://static.wikitide.net/commonswikitide/2/22/WikiTide_icon.svg" width="130" height="130" alt="WikiTide Logo" />
+				<img src="https://static.wikitide.net/commonswiki/2/22/WikiTide_icon.svg" width="130" height="130" alt="WikiTide Logo" />
 				<h1>Something went wrong</h1>
 				<p class="lead">Give it a bit and try again. <a href="https://static-help.wikitide.org/docs/errors/503">Learn more</a>.</p>
 				<a href="javascript:document.location.reload(true);" class="btn btn-outline-primary" role="button">Try this action again</a>
