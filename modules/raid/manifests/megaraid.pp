0157e4559d8bdff3604e79574fdc77a4253097ca
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-12-09 14:12:42 -0700

    Fix

--- modules/raid/manifests/megaraid.pp
+++ modules/raid/manifests/megaraid.pp
@@ -5,8 +5,8 @@ class raid::megaraid {
 
     stdlib::ensure_packages('megacli')
 
-    file { '/usr/lib/nagios/plugins/get_raid_status_megacli.py':
-        source => 'puppet:///modules/raid/get_raid_status_megacli',
+    file { '/usr/lib/nagios/plugins/get_raid_status_megacli':
+        source => 'puppet:///modules/raid/get_raid_status_megacli.py',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
