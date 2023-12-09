672f25ce93cf6c70eaccb7b2cdd175d64c96fe69
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-12-09 14:01:28 -0700

    Update

--- modules/raid/lib/facter/raid.rb
+++ modules/raid/lib/facter/raid.rb
@@ -6,11 +6,7 @@ Facter.add('raid_mgmt_tools') do
   # Run sudo lspci -nn to add missing PCI IDs, the combination of vendor ID and device ID is printed in []
   pci_ids = {
     '100010e2' => 'perccli',     # Broadcom / LSI MegaRAID 12GSAS/PCIe Secure SAS39xx (sold as Perc H750)
-    '1000005d' => 'megaraid',    # LSI Logic / Symbios Logic MegaRAID SAS-3 3108, also shows up as
-                                 # Broadcom / LSI MegaRAID SAS-3 3108 [Invader]
-    '100000cf' => 'megaraid',    # Broadcom / LSI MegaRAID SAS-3 3324 [Intruder] (rev 01)
-    '10000016' => 'megaraid',    # Broadcom / LSI MegaRAID Tri-Mode SAS3508
-    '10000014' => 'megaraid',    # LSI Logic / Symbios Logic MegaRAID Tri-Mode SAS3516
+    '1000005b' => 'megaraid',
   }
   setcode do
     raids = []
