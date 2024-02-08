4520d23486fd10d11f5270b722a4a301d29d7fe6
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-02-07 18:49:25 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,26 +1,11 @@
 # servers
 
-node /^cloud[1234]\.wikitide\.net$/ {
+node 'cloud1.wikitide.net' {
     include base
     include role::cloud
 }
 
-node 'bast1.wikitide.net' {
-    include base
-    include role::bastion
-}
-
-node 'bots1.wikitide.net' {
-    include base
-    include role::irc
-}
-
-node /^cp[12456]\.wikitide\.net$/ {
-    include base
-    include role::varnish
-}
-
-node 'cp3.wikitide.net' {
+node /^cp[456]\.wikitide\.net$/ {
     include base
     include role::varnish
 }
@@ -37,24 +22,9 @@ node 'jobchron1.wikitide.net' {
     include mediawiki::jobqueue::chron
 }
 
-node 'graylog1.wikitide.net' {
-    include base
-    include role::graylog
-}
-
 node 'jobrunner1.wikitide.net' {
     include base
     include role::mediawiki
-}
-
-node 'ldap1.wikitide.net' {
-    include base
-    include role::openldap
-}
-
-node 'matomo1.wikitide.net' {
-    include base
-    include role::matomo
 }
 
 node 'mem1.wikitide.net' {
@@ -62,30 +32,9 @@ node 'mem1.wikitide.net' {
     include role::memcached
 }
 
-node 'mon1.wikitide.net' {
-    include base
-    include role::grafana
-    include role::icinga2
-}
-
 node /^mw[12]\.wikitide\.net$/ {
     include base
     include role::mediawiki
-}
-
-node 'os1.wikitide.net' {
-    include base
-    include role::opensearch
-}
-
-node 'phorge1.wikitide.net' {
-    include base
-    include role::phorge
-}
-
-node 'prometheus1.wikitide.net' {
-    include base
-    include role::prometheus
 }
 
 node 'puppet1.wikitide.net' {
@@ -95,16 +44,6 @@ node 'puppet1.wikitide.net' {
     include role::puppetserver
     include role::salt
     include role::ssl
-}
-
-node 'reports1.wikitide.net' {
-    include base
-    include role::reports
-}
-
-node 'services1.wikitide.net' {
-    include base
-    include role::services
 }
 
 node 'swiftac1.wikitide.net' {
@@ -120,15 +59,6 @@ node 'swiftobject1.wikitide.net' {
 node 'swiftproxy1.wikitide.net' {
     include base
     include role::swift
-}
-
-node 'test1.wikitide.net' {
-    include base
-    include role::mediawiki
-    include role::memcached
-    include role::poolcounter
-    include role::redis
-    include mediawiki::jobqueue::chron
 }
 
 # ensures all servers have basic class if puppet runs
